366640257ad62f9d223a29dbaf4a3b237f5a342d
chris@prime <66511731+aioooi@users.noreply.github.com>
2020-08-22 16:49:40 +0200

    add global css

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Ok, let's got!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Let's Play: Tic Tac Toe</h1>
+      <h2>(implemented using React)</h2>
     </div>
   );
 }
