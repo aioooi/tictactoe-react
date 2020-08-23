1a45fab099a2dec3a9d1f17837dcd8fd646640a0
chris@prime <66511731+aioooi@users.noreply.github.com>
2020-08-23 12:55:29 +0200

    Square: basic structure and CSS

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,12 +1,33 @@
 import React from "react";
 
+import "./Square.css";
+
+import * as ttt from "../lib/tictactoe/tictactoe.js";
+
 class Square extends React.Component {
   constructor(props) {
     super(props);
     this.props = props;
   }
   render() {
-    return <p>{this.props.id}</p>
+    let tag = '';
+    if (this.props.owner === ttt.PLAYER) {
+      tag = <span>&#x0fbe;</span>;
+    } else if (this.props.owner === ttt.COMPUTER) {
+      tag = <span>&#x262f;</span>;
+    }
+
+    return (
+      <div
+        className={
+          "square " +
+          (this.props.highlight === true ? "highlight-winning-line " : " ") +
+          (this.props.owner !== ttt.EMPTY ? "played" : "")
+        }
+      >
+        <div className="tag">{tag}</div>
+      </div>
+    );
   }
 }
 
