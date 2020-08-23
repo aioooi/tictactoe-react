eeaabdba932244f50b20f978a7df8a6dec1cc33d
chris@prime <66511731+aioooi@users.noreply.github.com>
2020-08-23 12:55:02 +0200

    Game: use Square component

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,12 +1,31 @@
 import React from "react";
 
+import "./Game.css";
+
 import LevelSelection from "./LevelSelection.js";
-import Square from "./Square.js"
+import Square from "./Square.js";
 import Stats from "./Stats.js";
 
 class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.props = props;
+    this.state = {
+      state: [
+        [0, -1, 0],
+        [0, 1, 0],
+        [0, 0, 0],
+      ],
+    };
+  }
   render() {
-    let board = [...new Array(9)].map((v, i) => <Square id={i}></Square>);
+    let board = [...new Array(9)].map((v, i) => (
+      <Square
+        id={i}
+        owner={this.state.state[Math.floor(i / 3)][i % 3]}
+        highlight={false}
+      />
+    ));
     return (
       <div>
         <div className="board">{board}</div>
