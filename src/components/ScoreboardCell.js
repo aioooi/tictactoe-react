3146f1ff7a85c4ff92daf3d07e67163af6ea1a04
chris@prime <66511731+aioooi@users.noreply.github.com>
2020-08-24 18:14:29 +0200

    animate ScoreboardCell

--- src/components/ScoreboardCell.js
+++ src/components/ScoreboardCell.js
@@ -1,15 +1,27 @@
 import React from "react";
 
-import "./ScoreboardCell.css"
+import "./ScoreboardCell.css";
 
 class ScoreboardCell extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      classNames: "",
+    };
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.value !== prevProps.value) {
+      this.setState({ classNames: "scale-animation" });
+      setTimeout(() => {
+        this.setState({ classNames: "" });
+      }, 400);
+    }
   }
 
   render() {
     return (
-      <div>
+      <div className={this.state.classNames}>
         <div>{this.props.label}</div>
         <div>{this.props.value}</div>
       </div>
