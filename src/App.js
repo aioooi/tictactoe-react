2dcb5b6e6a27e24d497d4e9cd3333a102c40d8d3
chris@prime <66511731+aioooi@users.noreply.github.com>
2020-08-25 17:26:58 +0200

    fix margin of Game

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
 import "./App.css";
 
-import Game from  "./components/Game.js";
+import Game from "./components/Game.js";
 
 function App() {
   return (
     <div className="App">
       <h1>Let's Play: Tic Tac Toe</h1>
       <h2>(implemented using React)</h2>
-      <Game playerBegins={true} delay={300}/>
+      <div className="game">
+        <Game playerBegins={true} delay={300} />
+      </div>
     </div>
   );
 }
