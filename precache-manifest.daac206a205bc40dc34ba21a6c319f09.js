3a3926e5464a55d349b8cf3f6cf7e549f5e42d0e
chris@prime <66511731+aioooi@users.noreply.github.com>
2020-10-08 07:12:58 +0200

    Updates

---
+++ precache-manifest.daac206a205bc40dc34ba21a6c319f09.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e56e008b84809aabefc860c3b9ad511b",
+    "url": "/tictactoe-react/index.html"
+  },
+  {
+    "revision": "c023d0560a626f112a55",
+    "url": "/tictactoe-react/static/css/main.16b3095b.chunk.css"
+  },
+  {
+    "revision": "43762198e9eb566b0edd",
+    "url": "/tictactoe-react/static/js/2.4572e600.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tictactoe-react/static/js/2.4572e600.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c023d0560a626f112a55",
+    "url": "/tictactoe-react/static/js/main.c1b5f7d5.chunk.js"
+  },
+  {
+    "revision": "514f82430784f25a752b",
+    "url": "/tictactoe-react/static/js/runtime-main.a7ab193d.js"
+  },
+  {
+    "revision": "2da4b0ceed72ff1277eb20013388b7d6",
+    "url": "/tictactoe-react/static/media/pattern.2da4b0ce.png"
+  }
+]);
